905bba51955a97ccb785c93d3d1a53df5b53a69b
Edmond Soun <edmond.soun@gmail.com>
2022-08-05 15:19:43 -0700

    WIP through step 5

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 """Flask app for adopt app."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
+
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -31,3 +33,31 @@ def show_homepage():
     pets = Pet.query.all()
 
     return render_template('home.html', pets = pets)
+
+@app.route('/add', methods = ['GET','POST'])
+def handle_add_pet_form():
+    
+    form = AddPetForm()
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+    
+        new_pet = Pet(name=name,
+                      species=species,
+                      photo_url=photo_url,
+                      age=age,
+                      notes=notes)
+        
+        db.session.add(new_pet)
+        db.session.commit()
+        
+        #how to rerender form if doesn't validate?
+        
+        return redirect('/')
+        
+    else:
+        return render_template('add.html', form=form)
--- forms.py
+++ forms.py
@@ -1,1 +1,21 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, BooleanField, SelectField
+from wtforms.validators import InputRequired, Optional
+
+class AddPetForm(FlaskForm):
+
+    name = StringField('Name:', validators=[InputRequired()])
+    species = StringField('Species:', validators=[InputRequired()])
+    photo_url = StringField('Photo URL:', validators=[InputRequired()])
+    age = SelectField('Age:', choices=[('baby','Baby'),
+                                       ('young','Young'),
+                                       ('adult','Adult'),
+                                       ('senior','Senior')])
+                    # Way to force user to actively select?
+    notes = StringField('Notes:', validators=[Optional()])
+    
+    def validate_name(form, field):
+        if len(field.data) > 5:
+            raise ValidationError('Shorter input please!')
